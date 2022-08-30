52c33dba4d45abc7a75fe2c92118c9e00bed6a03
hao <hao@123.com>
2022-08-30 18:29:07 +0800

    code

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"dictionary-go/model"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello world!")
+
+	var (
+		from  string
+		to    string
+		query string
+	)
+	// go run .\main.go --from auto --to en --q 你好
+	flag.StringVar(&from, "from", "auto", "从那个语言翻译过来")
+	flag.StringVar(&to, "to", "en", "翻译语言")
+	flag.StringVar(&query, "q", "", "翻译的文本")
+	flag.Parse()
+	tran := model.NewTranslateModeler(query, from, to)
+	values := tran.ToValues()
+	resp, err := http.PostForm(model.Url, values) //调用一个post请求 并且上传表单
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer resp.Body.Close()
+	res := model.TransRespone{}            //初始化一个结构体接收返回消息
+	body, err := ioutil.ReadAll(resp.Body) //读取响应的响应体
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	json.Unmarshal(body, &res)    //反序列化到struct中
+	for _, v := range res.Trans { //输出源文本和目标文本
+		fmt.Println("原文本", v.Src)
+		fmt.Println("翻译文本", v.Dst)
+	}
+
 }
---
+++ model/model.go
@@ -0,0 +1,74 @@
+package model
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+//申请的信息  iD和密钥
+const (
+	UerAppID    = "20220816001309619"
+	UerPassword = "W_h0op7WCB9qcLUVl34B"
+)
+
+type TranslateModel struct {
+	Q     string
+	From  string
+	To    string
+	Appid string
+	Salt  int
+	Sign  string
+}
+
+type TransResult struct {
+	Src string `json:"src"`
+	Dst string `json:"dst"`
+}
+
+type TransRespone struct {
+	From  string        `json:"from"`
+	To    string        `json:"to"`
+	Trans []TransResult `json:"trans_result"`
+}
+
+//初始化一个翻译模块
+func NewTranslateModeler(q, from, to string) TranslateModel {
+	tran := TranslateModel{
+		Q:    q,
+		From: from,
+		To:   to,
+	}
+	tran.Appid = UerAppID
+	tran.Salt = time.Now().Second()
+	content := UerAppID + q + strconv.Itoa(tran.Salt) + UerPassword
+	sign := SumString(content) //计算sign值
+	tran.Sign = sign
+	return tran
+}
+
+//将对应的请求表单组装好
+func (tran TranslateModel) ToValues() url.Values {
+	values := url.Values{
+		"q":     {tran.Q},
+		"from":  {tran.From},
+		"to":    {tran.To},
+		"appid": {tran.Appid},
+		"salt":  {strconv.Itoa(tran.Salt)},
+		"sign":  {tran.Sign},
+	}
+	return values
+}
+
+//计算文本的md5值
+func SumString(content string) string {
+	bys := md5.Sum([]byte(content))
+	//bys := md5.Sum([]byte(content))//这个md5.Sum返回的是数组,不是切片哦
+	value := hex.EncodeToString(bys[0:]) //根据数组构造一个切片
+	return value
+}
+
+//百度翻译api接口
+var Url = "http://api.fanyi.baidu.com/api/trans/vip/translate"
